686f4d9146d7bf73ee883fec5f8e039e10031b14
JuanVarvello <118789279+JuanVarvello@users.noreply.github.com>
2023-04-04 23:21:53 -0300

    Update script.js

--- vd_s1_tp2_Varvello_Juan_Y_Agustina_Glusman/ocupacion/script.js
+++ vd_s1_tp2_Varvello_Juan_Y_Agustina_Glusman/ocupacion/script.js
@@ -1,27 +1,22 @@
 d3.csv("astronautas.csv", d3.autoType).then((data) => {
-  
-  let counts = d3.rollup(
-    data,
-    v => v.length, // Contar la cantidad de astronautas en cada grupo
-    d => d.ocupacion // Agrupar por ocupación
-  );
+  d3.select("#ocupacion-selector").on("change", () => {
+    let ocupacionSeleccionada = d3.select("#ocupacion-selector").property("value");
+    let dataFiltrada = data;
+    //console.log(ocupacionSeleccionada)
+    if (ocupacionSeleccionada !== "default") {
+      dataFiltrada = data.filter((d) => d.ocupacion === ocupacionSeleccionada);
+    }
+    let chart = Plot.plot({
+      marks: [Plot.dot(dataFiltrada, { x: "ocupacion", y: "nacionalidad", fill: "ocupacion" })],
+      grid: true,
+      line: true,
+      nice: true,
+      width: 900,
+      height: 350,
+      marginLeft: 130,
+    });
 
-  let chart = Plot.plot({
-    width: 750,
-    height: 600,
-    marks: [
-      Plot.barY(counts, {
-  
-        x: d => d[0], // Usar la ocupación como eje x
-        y: d => d[1], // Usar la cantidad como eje y
-        fill: d => d[0], // Usar la ocupación como color
-      }),
-    ],
-    grid: true,
-    line: false, // Deshabilitar las líneas de la cuadrícula
-    nice: true,
-    color: true,
+    d3.select("#chart").html("");
+    d3.select("#chart").append(() => chart);
   });
-
-  d3.select("#chart").append(() => chart);
 });
